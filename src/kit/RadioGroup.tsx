1c697529800a6525f37fd35515a7a1aef7fed3ab
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-10-20 15:53:39 -0300

    chore: remove hardcoded code

--- src/kit/RadioGroup.tsx
+++ src/kit/RadioGroup.tsx
@@ -1,10 +1,8 @@
 import { Radio } from 'antd';
 import { RadioChangeEvent } from 'antd/lib/radio';
 import React, {
-  RefObject,
   useCallback,
   useEffect,
-  useLayoutEffect,
   useMemo,
   useRef,
   useState,
@@ -12,56 +10,12 @@ import React, {
 import { throttle } from 'throttle-debounce';
 
 import Icon, { IconName, IconSize } from 'kit/Icon';
+import useResize from 'kit/internal/useResize';
 import Tooltip from 'kit/Tooltip';
 
 import css from './RadioGroup.module.scss';
 
-interface ResizeInfo {
-  height: number;
-  width: number;
-  x: number;
-  y: number;
-}
-
-const defaultResizeInfo = {
-  height: 0,
-  width: 0,
-  x: 0,
-  y: 0,
-};
-
 export const DEFAULT_RESIZE_THROTTLE_TIME = 500;
-
-const useResize = (ref?: RefObject<HTMLElement>): ResizeInfo => {
-  const [resizeInfo, setResizeInfo] = useState<ResizeInfo>(defaultResizeInfo);
-
-  useLayoutEffect(() => {
-    let element = document.body;
-    if (ref) {
-      if (ref.current) element = ref.current;
-      else return;
-    }
-
-    const handleResize: ResizeObserverCallback = (entries: ResizeObserverEntry[]) => {
-      // Check to make sure the ref container is being observed for resize.
-      const elements = entries.map((entry: ResizeObserverEntry) => entry.target);
-      if (!element || elements.indexOf(element) === -1) return;
-
-      const rect = element.getBoundingClientRect();
-      setResizeInfo(rect);
-    };
-    const resizeObserver = new ResizeObserver(handleResize);
-    resizeObserver.observe(element);
-
-    // Set initial resize info
-    const rect = element.getBoundingClientRect();
-    setResizeInfo(rect);
-
-    return (): void => resizeObserver.unobserve(element);
-  }, [ref]);
-
-  return resizeInfo;
-};
 
 interface Props {
   defaultValue?: string;
