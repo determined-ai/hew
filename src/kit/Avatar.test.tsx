0257d12fe46558e84c49b704c002af3b8368ee37
julian-determined-ai <julian.glover@hpe.com>
2023-10-24 12:03:50 -0500

    fix tests

--- src/kit/Avatar.test.tsx
+++ src/kit/Avatar.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
-import { ThemeProvider } from 'kit/Theme';
+import { ThemeProvider, UIProvider } from 'kit/Theme';
 
 import Avatar, { Props } from './Avatar';
 
@@ -12,7 +12,9 @@ const user = userEvent.setup();
 const setup = ({ displayName, hideTooltip = false, ...props }: Props) => {
   render(
     <ThemeProvider>
-      <Avatar displayName={displayName} hideTooltip={hideTooltip} {...props} />
+      <UIProvider>
+        <Avatar displayName={displayName} hideTooltip={hideTooltip} {...props} />
+      </UIProvider>
     </ThemeProvider>,
   );
 };
