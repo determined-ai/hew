9b1d221504de2da2fa7aed6ae2414dfb220c1fbe
julian-determined-ai <julian.glover@hpe.com>
2023-10-25 11:31:36 -0500

    formatting

--- src/DesignKit.tsx
+++ src/DesignKit.tsx
@@ -67,7 +67,7 @@ import loremIpsum, { loremIpsumSentence } from 'utils/loremIpsum';
 import css from './DesignKit.module.scss';
 import ThemeToggle from './ThemeToggle';
 
-const noOp = () => { };
+const noOp = () => {};
 
 const handleError: ErrorHandler = () =>
   makeToast({
