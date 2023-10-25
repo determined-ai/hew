a17752fcfe2daf62bbe19b4def6f3d79b0e8a57f
gt2345 <gt2345@columbia.edu>
2023-10-25 14:28:40 -0500

    resolve imports in test

--- src/kit/Badge.test.tsx
+++ src/kit/Badge.test.tsx
@@ -1,11 +1,7 @@
-import { badgeColorFromState, stateToLabel } from 'constants/states';
-
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { UIProvider } from 'components/kit/Theme';
-import React, { useState } from 'react';
-import { ResourceState, SlotState } from 'types';
-import { generateAlphaNumeric } from 'utils/string';
+import { UIProvider } from 'kit/Theme';
+import { generateAlphaNumeric } from 'kit/internal/functions';
 
 import Badge, { BadgeProps } from './Badge';
 
@@ -27,32 +23,4 @@ describe('Badge', () => {
     expect(view.getByText(CONTENT)).toBeInTheDocument();
   });
 
-  it('should display dynamic content from state prop', async () => {
-    const TestComponent = () => {
-      const [value, setValue] = useState<SlotState>(SlotState.Free);
-      return (
-        <UIProvider>
-          <button role="button" onClick={() => setValue(SlotState.Running)} />
-          <Badge backgroundColor={badgeColorFromState(value)} text={stateToLabel(value)} />
-        </UIProvider>
-      );
-    };
-    const view = render(<TestComponent />);
-
-    await user.click(view.getByRole('button'));
-
-    await waitFor(() => {
-      expect(view.getByText(stateToLabel(SlotState.Running))).toBeInTheDocument();
-    });
-  });
-
-  it('should display correct style for potential', () => {
-    const label = stateToLabel(ResourceState.Potential);
-    const view = setup({
-      dashed: true,
-      text: label,
-    });
-    const statePotential = view.getByText(label);
-    expect(statePotential).toHaveClass('base dashed');
-  });
 });
