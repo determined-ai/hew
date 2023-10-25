8e7c453ef0a1ba02fc11b66a799953e81bd271fb
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-10-25 13:25:07 -0300

    chore: attend to PR comments

--- src/kit/RadioGroup.tsx
+++ src/kit/RadioGroup.tsx
@@ -1,7 +1,6 @@
 import { Radio } from 'antd';
 import { RadioChangeEvent } from 'antd/lib/radio';
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { throttle } from 'throttle-debounce';
 
 import Icon, { IconName, IconSize } from 'kit/Icon';
 import useResize from 'kit/internal/useResize';
@@ -34,7 +33,6 @@ interface SizeInfo {
   parentWidth: number;
 }
 
-const RESIZE_THROTTLE_TIME = 500;
 const PARENT_WIDTH_BUFFER = 16;
 const HEIGHT_LIMIT = 50;
 
@@ -49,17 +47,22 @@ const RadioGroup: React.FC<Props> = ({
   const baseRef = useRef<HTMLDivElement>(null);
   const originalWidth = useRef<number>();
   const { refCallback, size } = useResize();
-  const [showLabels, setShowLabels] = useState(true);
   const [sizes, setSizes] = useState<SizeInfo>({ baseHeight: 0, baseWidth: 0, parentWidth: 0 });
   const classes = [css.base];
-
-  refCallback(baseRef.current);
 
   const hasIconsAndLabels = useMemo(() => {
     if (options.length === 0) return false;
     return options.reduce((acc, option) => acc || (!!option.icon && !!option.label), false);
   }, [options]);
 
+  const showLabels = useMemo(() => {
+    if (!hasIconsAndLabels || !baseRef.current) return true;
+    if (sizes.baseWidth === 0 || sizes.parentWidth === 0) return true;
+    if (originalWidth.current && originalWidth.current < sizes.parentWidth) return true;
+    if (sizes.baseHeight > HEIGHT_LIMIT) return false;
+
+    return true;
+  }, [hasIconsAndLabels, sizes]);
   if (iconOnly) classes.push(css.iconOnly);
 
   const handleChange = useCallback(
@@ -69,46 +72,28 @@ const RadioGroup: React.FC<Props> = ({
     [onChange],
   );
 
-  /*
-   * Dynamically check to see if labels can be shown along with the icons,
-   * if there's room to show both and both are available.
-   */
-  useEffect(() => {
-    if (!hasIconsAndLabels || !baseRef.current) return;
-    if (sizes.baseWidth === 0 || sizes.parentWidth === 0) return;
-
-    setShowLabels((prev) => {
-      if (!originalWidth.current) return prev;
-      if (prev && sizes.baseHeight > HEIGHT_LIMIT) return false;
-      if (!prev && originalWidth.current < sizes.parentWidth) return true;
-      return prev;
-    });
-  }, [hasIconsAndLabels, sizes]);
+  useEffect(() => refCallback(baseRef.current), [refCallback]);
 
   /*
    * Update parent and component sizes upon resize of the window,
    * at a throttled rate.
    */
   useEffect(() => {
-    const throttleFunc = throttle(RESIZE_THROTTLE_TIME, () => {
-      if (!hasIconsAndLabels || !baseRef.current) return;
-      const parent = baseRef.current.parentElement;
-      if (!parent) return;
+    if (!hasIconsAndLabels || !baseRef.current) return;
+    const parent = baseRef.current.parentElement;
+    if (!parent) return;
 
-      const parentRect = parent.getBoundingClientRect();
-      if (!parentRect) return;
+    const parentRect = parent.getBoundingClientRect();
+    if (!parentRect) return;
 
-      const baseRect = size;
-      if (!originalWidth.current) originalWidth.current = baseRect.width;
+    const baseRect = size;
+    if (!originalWidth.current) originalWidth.current = baseRect.width;
 
-      setSizes({
-        baseHeight: baseRect.height,
-        baseWidth: baseRect.width,
-        parentWidth: parentRect.width - PARENT_WIDTH_BUFFER,
-      });
+    setSizes({
+      baseHeight: baseRect.height,
+      baseWidth: parentRect.width,
+      parentWidth: parentRect.width - PARENT_WIDTH_BUFFER,
     });
-
-    throttleFunc();
   }, [hasIconsAndLabels, size]);
 
   return (
