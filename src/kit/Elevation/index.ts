d80cb0b337a033dccd72eca0351cfefaf15fe240
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-10-27 17:50:19 -0300

    feat: change the enums

--- src/kit/Elevation/index.ts
+++ src/kit/Elevation/index.ts
@@ -1,35 +1,63 @@
-export const ElevationDark = {
-  elevation0Bg: '#1C1C1C',
-  elevation0Border: '#323232',
-  elevation0Hover: '#1C1C1C',
-  elevation1Bg: '#242424',
-  elevation1Border: '#323232',
-  elevation1Hover: '#323232',
-  elevation2Bg: '#2A2A2A',
-  elevation2Border: '#3A3A3A',
-  elevation2Hover: '#303030',
-  elevation3Bg: '#323232',
-  elevation3Border: '#3A3A3A',
-  elevation3Hover: '#303030',
-  elevation4Bg: '#383838',
-  elevation4Border: '#3A3A3A',
-  elevation4Hover: '#303030',
+export const Elevation0 = {
+  dark: {
+    bg: '#1C1C1C',
+    border: '#323232',
+    hover: '#1C1C1C',
+  },
+  light: {
+    bg: '#F0F0F0',
+    border: '#E2E2E2',
+    hover: '#ECECEC',
+  },
+} as const;
+export const Elevation1 = {
+  dark: {
+    bg: '#242424',
+    border: '#323232',
+    hover: '#323232',
+  },
+  light: {
+    bg: '#F7F7F7',
+    border: '#E2E2E2',
+    hover: '#F5F5F5',
+  },
 } as const;
 
-export const ElevationLight = {
-  elevation0Bg: '#F0F0F0',
-  elevation0Border: '#E2E2E2',
-  elevation0Hover: '#ECECEC',
-  elevation1Bg: '#F7F7F7',
-  elevation1Border: '#E2E2E2',
-  elevation1Hover: '#F5F5F5',
-  elevation2Bg: '#FDFDFD',
-  elevation2Border: '#E8E8E8',
-  elevation2Hover: '#F8F8F8',
-  elevation3Bg: '#FFF',
-  elevation3Border: '#E8E8E8',
-  elevation3Hover: '#F8F8F8',
-  elevation4Bg: '#FFF',
-  elevation4Border: '#E8E8E8',
-  elevation4Hover: '#F8F8F8',
+export const Elevation2 = {
+  dark: {
+    bg: '#2A2A2A',
+    border: '#3A3A3A',
+    hover: '#303030',
+  },
+  light: {
+    bg: '#FDFDFD',
+    border: '#E8E8E8',
+    hover: '#F8F8F8',
+  },
+} as const;
+
+export const Elevation3 = {
+  dark: {
+    bg: '#323232',
+    border: '#3A3A3A',
+    hover: '#303030',
+  },
+  light: {
+    bg: '#FFF',
+    border: '#E8E8E8',
+    hover: '#F8F8F8',
+  },
+} as const;
+
+export const Elevation4 = {
+  dark: {
+    bg: '#383838',
+    border: '#3A3A3A',
+    hover: '#303030',
+  },
+  light: {
+    bg: '#FFF',
+    border: '#E8E8E8',
+    hover: '#F8F8F8',
+  },
 } as const;
--- src/kit/Theme/themeUtils.ts
+++ src/kit/Theme/themeUtils.ts
@@ -1,5 +1,4 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
-import { ElevationDark, ElevationLight } from 'kit/Elevation';
 import { isColor, rgba2str, rgbaMix, str2rgba } from 'kit/internal/color';
 import { ValueOf } from 'kit/utils/types';
 
@@ -199,7 +198,21 @@ const themeLight = {
   elevationWeak: '0px 2px 4px rgba(0, 0, 0, 0.24)',
 
   // Elevation styles
-  ...ElevationLight,
+  elevation0Bg: '#F0F0F0',
+  elevation1Bg: '#F7F7F7',
+  elevation2Bg: '#FDFDFD',
+  elevation3Bg: '#FFF',
+  elevation4Bg: '#FFF',
+  elevation0Hover: '#ECECEC',
+  elevation1Hover: '#F5F5F5',
+  elevation2Hover: '#F8F8F8',
+  elevation3Hover: '#F8F8F8',
+  elevation4Hover: '#F8F8F8',
+  elevation0Border: '#E2E2E2',
+  elevation1Border: '#E2E2E2',
+  elevation2Border: '#E8E8E8',
+  elevation3Border: '#E8E8E8',
+  elevation4Border: '#E8E8E8',
 };
 
 const themeDark = {
@@ -245,7 +258,21 @@ const themeDark = {
   elevationWeak: '0px 2px 4px rgba(255, 255, 255, 0.12)',
 
   // Elevation styles
-  ...ElevationDark,
+  elevation0Bg: '#1C1C1C',
+  elevation1Bg: '#242424',
+  elevation2Bg: '#2A2A2A',
+  elevation3Bg: '#323232',
+  elevation4Bg: '#383838',
+  elevation0Hover: '#1C1C1C',
+  elevation1Hover: '#323232',
+  elevation2Hover: '#303030',
+  elevation3Hover: '#303030',
+  elevation4Hover: '#303030',
+  elevation0Border: '#323232',
+  elevation1Border: '#323232',
+  elevation2Border: '#3A3A3A',
+  elevation3Border: '#3A3A3A',
+  elevation4Border: '#3A3A3A',
 };
 
 export const themeLightDetermined: Theme = generateStrongWeak(
