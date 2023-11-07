b1c74a9287bf9a6f31d2f55f1399a2e0e5b05284
julian-determined-ai <julian.glover@hpe.com>
2023-11-07 11:17:18 -0600

    cleanup

--- src/kit/Icon/Spin.tsx
+++ src/kit/Icon/Spin.tsx
@@ -18,8 +18,5 @@ const Spin: React.FC<Props> = ({ type }) => {
 export const SpinBowtieComponent: React.FC = () => <Spin type="bowtie" />;
 export const SpinHalfComponent: React.FC = () => <Spin type="half" />;
 export const SpinShadowComponent: React.FC = () => <Spin type="shadow" />;
-export const SpinBowtie = SpinBowtieComponent;
-export const SpinHalf = SpinHalfComponent;
-export const SpinShadow = SpinShadowComponent;
 
 export default Spin;
