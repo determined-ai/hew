fc8155f002b5389d6dc42e5eb2b527420d95f073
John Kim <john.kim@hpe.com>
2023-12-01 12:27:24 -0500

    fix useResize

--- src/kit/useMobile.tsx
+++ src/kit/useMobile.tsx
@@ -1,12 +1,12 @@
-import useResize from './internal/useResizeWidth';
+import useResize from './internal/useResize';
 import styles from './scss/breakpoints.module.scss';
 
 export const MOBILE_BREAKPOINT = parseInt(styles.breakpointMobile.split('px')[0]);
 
 const useMobile = (): boolean => {
-  const { width } = useResize();
+  const { size } = useResize();
 
-  return width < MOBILE_BREAKPOINT;
+  return size.width < MOBILE_BREAKPOINT;
 };
 
 export default useMobile;
