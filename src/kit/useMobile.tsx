cf8960129132c9bd175bef2264ee9726668e2fb8
John Kim <john.kim@hpe.com>
2023-12-01 17:05:32 -0500

    refactor useMobile

--- src/kit/useMobile.tsx
+++ src/kit/useMobile.tsx
@@ -1,12 +1,22 @@
-import useResize from './internal/useResize';
+import { useEffect, useState } from 'react';
+
 import styles from './scss/breakpoints.module.scss';
 
 export const MOBILE_BREAKPOINT = parseInt(styles.breakpointMobile.split('px')[0]);
 
-const useMobile = (): boolean => {
-  const { size } = useResize();
+const getIsMobile = () => window.innerWidth <= MOBILE_BREAKPOINT;
 
-  return size.width < MOBILE_BREAKPOINT;
+const useMobile = (): boolean => {
+  const [isMobile, setIsMobile] = useState(getIsMobile);
+  useEffect(() => {
+    const onResize = () => setIsMobile(getIsMobile);
+    window.addEventListener('resize', onResize);
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  return isMobile;
 };
 
 export default useMobile;
