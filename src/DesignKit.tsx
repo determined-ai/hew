761d2b75301fb92e41c041a671acb77f24442c87
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-12-04 17:25:44 -0300

    attend to PR comments

--- src/DesignKit.tsx
+++ src/DesignKit.tsx
@@ -2562,7 +2562,7 @@ const LogViewerSection: React.FC = () => {
       </AntDCard>
       <AntDCard title="Usage">
         <strong>LogViewer default</strong>
-        <div style={{ height: '350px' }}>
+        <div style={{ minHeight: '370px' }}>
           <LogViewer
             decoder={(l) => l as Log}
             initialLogs={sampleLogs}
