789a1e04c30134cb18de38978cfdb653e1fa97f8
Ashton Galloway <ashton.galloway@hpe.com>
2024-10-02 13:22:52 -0400

    chore: introduce useMediaQuery
    this introduces `useMediaQuery`, a hook that devs can use to listen to
    the status of a media query. we also replace `useMobile` with this as a
    wrapper, and explain why we're not replacing `useSystemMode` with it.

--- src/kit/useMobile.tsx
+++ src/kit/useMobile.tsx
@@ -1,22 +1,8 @@
-import { useEffect, useState } from 'react';
-
 import styles from './scss/breakpoints.module.scss';
+import { useMediaQuery } from './utils/useMediaQuery';
 
 export const MOBILE_BREAKPOINT = styles.breakpointMobile;
 
-const queries = window.matchMedia(`only screen and (max-width: ${MOBILE_BREAKPOINT})`);
-
-const useMobile = (): boolean => {
-  const [isMobile, setIsMobile] = useState(queries.matches);
-  useEffect(() => {
-    const onChange = () => setIsMobile(queries.matches);
-    queries.addEventListener('change', onChange);
-    return () => {
-      queries.removeEventListener('change', onChange);
-    };
-  }, []);
-
-  return isMobile;
-};
+const useMobile = (): boolean => useMediaQuery(`only screen and (max-width: ${MOBILE_BREAKPOINT})`);
 
 export default useMobile;
